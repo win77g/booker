b89c387d6f52b05d70cbcd7e03d9b340f8a4a951
win77g <46159762+win77g@users.noreply.github.com>
2020-01-23 21:07:22 +0300

    add "os.path.join(SETTINGS_PATH, 'templates')"

--- dj_backend/settings.py
+++ dj_backend/settings.py
@@ -74,7 +74,7 @@ ROOT_URLCONF = 'dj_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(SETTINGS_PATH, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
