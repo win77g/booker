0bef4f645dd1716fb21130e564ffddacea8abb10
win21g <win21g@mail.ru>
2020-01-25 15:46:20 +0300

    file

--- booker/settings.py
+++ booker/settings.py
@@ -155,10 +155,10 @@ REST_FRAMEWORK = {
     # 'PAGE_SIZE':10,
 
     'DEFAULT_AUTHENTICATION_CLASSES':(
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
     ),
 
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
